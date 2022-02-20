c47241de9c1607a29ccb9e5d4be67a7c30d85c23
Fernando Costa Leite - mister-storm <clfernando1979@hotmail.com>
2022-02-20 18:54:30 -0300

    finish security

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user(id, password, user_name) values(1, '$2a$12$tLHnuI9.jZP3JqJs5GoBxeObcbe4IfYzsanotbk/LQUtnYRq5uByW', 'anUser');
+insert into user(id, password, user_name) values(2, '$2a$12$eGa0uSu5k/ZYsTM9GKaMBOVLjurmmhyEQyGHrd6YJekXT2qxmUKSS', 'OtherUser');
+insert into role values (1,1);
+insert into role values (1,2);
+insert into role values (2,1);
+insert into role values (2,2);
+
